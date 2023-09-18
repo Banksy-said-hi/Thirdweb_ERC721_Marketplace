152ff9400f964ca24557395f9750f8e17adbdb3c
Sina Rahimi <sinarahimi.main@gmail.com>
2023-09-18 22:51:55 +0200

    typo fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import { Navbar } from "../components/navbar";
+import { Navbar } from "../components/Navbar";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
